e819655a057a249ee60bc815df4a68ed6a34649d
oluwa Ibrahim <mj9762651@gmail.com>
2025-09-01 23:40:17 +0100

    style: implement basic styling to sectionNabBar

--- src/components/SectionNavBar.jsx
+++ src/components/SectionNavBar.jsx
@@ -1,16 +1,19 @@
-import {  NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const SectionNavBar = ({ views = [] }) => {
   return (
     <nav>
-      <ul className="flex bg-gray-100 w-max space-x-3 [&_.active]:bg-[#0373f3]">
+      <ul
+        className="flex bg-gray-100 w-max  [&_.active]:bg-[#0373f3]
+       rounded-md [&_a]:rounded-md  [&_.active]:text-white [&_a]:text-sm mx-auto [&_a]:block
+       [&_a]:text-center [&_a]:w-20 [&_a]:px-2 [&_a]:transition-all [&_a]:duration-350 [&_a]:ease-in-out [&>li]:p-1"
+      >
         <li>
           <NavLink to={views[0]}>{views[0]}</NavLink>
         </li>
         <li>
           <NavLink to={views[1]}>{views[1]}</NavLink>
         </li>
-        
       </ul>
     </nav>
   );
