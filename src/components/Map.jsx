b99ece526f6da56184a407e9415159fed9ee41a4
oluwa Ibrahim <mj9762651@gmail.com>
2025-09-02 14:50:58 +0100

    feat(map): install Leaflet and render initial map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,20 @@
+import {MapContainer ,TileLayer, Marker, Popup} from "react-leaflet"
 const Map = () => {
-  return <div>Map</div>;
+  return (
+    <div className="h-screen">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="h-full">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Map;
